0a7c18980c6ac00e88b8a879682bc92e7dfa45b4
Lukasz Kaczynski <ukaczyski6@gmail.com>
2022-04-28 20:07:56 +0200

    Added Local.java
    Klasa zawiera metody zwracajace lokalna date i czas

---
+++ LABS/src/com/lab1_a/Local.java
@@ -0,0 +1,28 @@
+package com.lab1_a;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Klasa zawiera metody zwracajace lokalna date i czas
+ */
+public class Local {
+    /**
+     *
+     * @return data w formacie (yyyy-mm-dd)
+     */
+    public static LocalDate getCurrentDate(){
+        return LocalDate.now();
+    }
+
+    /**
+     *
+     * @return ciag znakow przechowujacy czas w formacie (HH:mm)
+     */
+    public static String getLocalTime(){
+        LocalTime today = LocalTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+        return today.format(dtf);
+    }
+}
