d7f18c496a1fd0cd6bbc1fdab0909d86c17b0243
Lukasz Kaczynski <ukaczyski6@gmail.com>
2022-04-28 20:18:48 +0200

    Create Address.java
    Describes a mailing address.

---
+++ LABS/src/com/lab3b/Address.java
@@ -0,0 +1,64 @@
+package com.lab3b;
+
+/**
+ * Describes a mailing address.
+ */
+public class Address {
+
+    private String street;
+    private String zipcode;
+    private String city;
+
+    /**
+     * Creates a mailing address.
+     * @param street  the street
+     * @param zipcode the zip postal code
+     * @param city    the city
+     */
+    public Address(String street, String zipcode, String city){
+        this.street = street;
+        this.zipcode = zipcode;
+        this.city = city;
+    }
+
+    // getters and setters
+    public String getStreet(){
+        return this.street;
+    }
+
+    public String getZipcode(){
+        return this.zipcode;
+    }
+
+    public String getCity(){
+        return this.city;
+    }
+
+    public void setStreet(String street){
+        this.street = street;
+    }
+
+    public void setZipcode(String zipcode){
+        this.zipcode = zipcode;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+
+    /**
+     * formats the address in single line
+     * @return a string containing the address
+     */
+    @Override
+    public String toString() {
+        return street + ", " + zipcode + " " + city ;
+    }
+    /**
+     * formats the address for mailing
+     * @return a string containing the address in mailing list format
+     */
+    public String formatForMailing(){
+        return street + "\n" + zipcode + " " + city;
+    }
+}
